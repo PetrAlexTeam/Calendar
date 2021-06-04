565a6de0026733bb00012c03136fe3a2f2e9acff
Petr5 <petr.mavzovin@mail.ru>
2021-06-04 18:03:36 +0300

    Оно грузится, Стили придется выучить

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -120,7 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATISFILES_DIRS = [
+    os.path.join(BASE_DIR, "Calendar\\static")
+]
+#print(os.path.join(BASE_DIR, "Calendar\static"))
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

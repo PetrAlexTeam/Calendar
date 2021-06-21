d53ba9899b8f46331a75c1feaec3837d5841b1ac
freQuensy23-coder <mametevalex@gmail.com>
2021-06-21 18:18:49 +0300

    Добавил test_create_calendar

--- Calendar/tests.py
+++ Calendar/tests.py
@@ -6,11 +6,16 @@ from .models import Task, Calendar
 
 # Create your tests here.
 
-class TasksTests(TestCase):
+class ModelsTests(TestCase):
     def setUp(self):
-        self.cal = Calendar.objects.create(name="Test_calendar", description="Tester", author="TestClass")
+        self.test_cal_name = "Test_calendar"
+        self.cal = Calendar.objects.create(name=self.test_cal_name, description="Tester", author="TestClass")
         self.e_task = Task.objects.create(name="Test_task_1", description="Test task 12:00 15 Jan 2021", author="TestClass", timestamp=1610701200, calendar=self.cal)
 
-    def test_easy_date(self):
+    def test_create_calendar(self):
+        cals = Calendar.objects.filter(name=self.test_cal_name).all()
+        self.assertIn(self.cal, cals)
+
+    def test_easy_task_date(self):
         tasks_15_jan = Task.get_day_tasks(date=datetime.date(day=15, month=1, year=2021), calendar=self.cal)
         self.assertIn(self.e_task, tasks_15_jan)

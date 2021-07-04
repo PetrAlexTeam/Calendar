cd327584015076b527ad7e1d34e1798c8216974a
freQuensy23-coder <mametevalex@gmail.com>
2021-07-04 11:29:25 +0300

    Изменил формачку добавления новой задачи, добавил туда возможность создавать повторяющиеся задачи

--- Calendar/forms.py
+++ Calendar/forms.py
@@ -16,26 +16,32 @@ class NewCalendarForm(ModelForm):
 
 class AddTaskForm(forms.Form):
     name = forms.CharField(label='Task title', max_length=63)
-    description = forms.CharField(label='Short Description', max_length=255)
-    author = forms.CharField(label='Creator', max_length=63)
+    description = forms.CharField(label='Short Description', max_length=255, required=False)
+    author = forms.CharField(label='Creator', max_length=63, required=False)
     date = forms.CharField(widget=forms.TextInput(attrs={"type": "datetime-local"}))
+    CHOICES = [(None, "No repeat"), (1, "Every day"), (7, "Every week")]
+    repeat_at = forms.ChoiceField(widget=forms.RadioSelect, label="repeat task", choices=CHOICES, required=False)
 
     def save(self, calendar, timezone=None):
+        from_date = datetime.datetime.strptime(self.cleaned_data['date'], "%Y-%m-%dT%H:%M")
+        from_date.replace(tzinfo=timezone)
+
         task = Task()
-        user_date = datetime.datetime.strptime(self.cleaned_data['date'], "%Y-%m-%dT%H:%M")
-        user_date.replace(tzinfo=timezone)
-        task.date = user_date
         task.name = self.cleaned_data['name']
         task.description = self.cleaned_data['description']
         try:
             task.author = self.cleaned_data['author']
         except KeyError:
             task.author = ""
-        task.timestamp = user_date.timestamp()
-        task.year = user_date.year
-        task.month = user_date.month
-        task.day = user_date.day
         task.calendar = calendar
-        task.hour = user_date.hour
-        task.minute = user_date.minute
-        task.save()
+        if self.cleaned_data.get("repeat_at"):
+            task.abstract_task = True
+            task.save()
+            period = datetime.timedelta(days=int(self.cleaned_data["repeat_at"]))
+            Task.repeated.create_repeated_tasks(task, period, start_date=from_date)
+        else:
+            # If task is not repeating
+            task.date = from_date
+            task.timestamp = from_date.timestamp()
+            task.save()
+
